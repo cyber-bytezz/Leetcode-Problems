4966bc5a074c52f3b167a72c7981d159c99ed431
Aro Barath Chandru <130319315+cyber-bytezz@users.noreply.github.com>
2024-01-25 00:50:40 +0530

    Update 3sum.py

--- 3Sum/3sum.py
+++ 3Sum/3sum.py
@@ -1,21 +1,27 @@
-def threeSum(self, nums):
-    res = []
-    nums.sort()
-    for i in xrange(len(nums)-2):
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
-        l, r = i+1, len(nums)-1
-        while l < r:
-            s = nums[i] + nums[l] + nums[r]
-            if s < 0:
-                l +=1 
-            elif s > 0:
-                r -= 1
-            else:
-                res.append((nums[i], nums[l], nums[r]))
-                while l < r and nums[l] == nums[l+1]:
-                    l += 1
-                while l < r and nums[r] == nums[r-1]:
+class Solution(object):
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            a = nums[i]
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                total_sum = a + nums[l] + nums[r]
+
+                if total_sum > 0:
                     r -= 1
-                l += 1; r -= 1
-    return res
+                elif total_sum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
+        return res
